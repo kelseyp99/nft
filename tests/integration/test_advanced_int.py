7d873c783923236f20e85fec96b08f3df331143b
kelseyp99 <werkhardor@gmail.com>
2022-01-11 19:11:36 -0500

    first commit

---
+++ tests/integration/test_advanced_int.py
@@ -0,0 +1,32 @@
+import pytest
+from brownie import network, AdvancedCollectible
+from scripts.helpful_scripts import (
+    get_account,
+    get_contract,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+import time
+
+
+def test_can_create_advanced_collectible_integration(
+#    get_keyhash,
+ #   chainlink_fee,
+):
+    # Arrange
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for integration testing")
+    #advanced_collectible = AdvancedCollectible.deploy(
+     #   get_contract("vrf_coordinator").address,
+      #  get_contract("link_token").address,
+       # get_keyhash,
+       # {"from": get_account()},
+    #)
+    #get_contract("link_token").transfer(
+    #    advanced_collectible.address, chainlink_fee * 3, {"from": get_account()}
+    #)
+    # Act
+    #advanced_collectible.createCollectible("None", {"from": get_account()})
+    advanced_collectible, creation_transaction = deploy_and_create()
+    time.sleep(60)
+    # Assert
+    assert advanced_collectible.tokenCounter() == 1
